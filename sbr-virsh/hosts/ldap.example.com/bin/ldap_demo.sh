3041b7c292906dbbb7e472818f51d89d7e70655e
Radomir Ludva <noreplay@radmi.cz>
2025-12-06 23:29:26 +0100

    Minor improvment for ldap_demo.sh script..

--- sbr-virsh/hosts/ldap.example.com/bin/ldap_demo.sh
+++ sbr-virsh/hosts/ldap.example.com/bin/ldap_demo.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Colors for output..
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
+
 #
 BASEDIR="$(cd "$(dirname "$0")/.." && pwd)"
 
